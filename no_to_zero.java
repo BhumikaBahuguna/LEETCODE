5f00dbea3a5b0fd00d294da606598ca9b67a5a3e
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2024-11-21 22:20:22 +0530

    no_to_zero.java

---
+++ no_to_zero.java
@@ -0,0 +1,16 @@
+1342 STEPS TO REDUCE A NUMBER TO ZERO
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int s=0;
+        while(num>0){
+            if((num & 1)==0){
+                num >>=1;
+            }else{
+                num--;
+            }
+            s++;
+        }
+        return s;
+    }
+}
